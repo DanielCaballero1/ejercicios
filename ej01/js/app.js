2bbc79947d28918cdf5c7c5109c7750e49c65f58
3a8i3 <adavidruizdiaz@gmail.com>
2019-05-20 19:44:37 -0400

    solucion de error y comentarios

--- ej01/js/app.js
+++ ej01/js/app.js
@@ -1,19 +1,33 @@
-$("#btn").click( () => {
-	
-	let cot = $("#cotizacion").val();
-	let mon = $("#monto").val();
-	let cam = cot * mon;
-	
-	let rs = {cot,mon,cam};
-	
-	cargar(rs);
+
+//evento click del boton
+$("#btn").click( function(){
+   // se recupera lo ingresado en el primer campo y se guarda en la variable
+   let cot = $("#cotizacion").val();
+
+   // se recupera lo ingresado en el segundo campo y se guarda en la variable
+   let mon = $("#monto").val();
+		
+   
+   // los valores se pasan a la funcion para realizar los calculos
+   calcular(cot, mon);
 	
 });
 
-cargar = a => {
 
-	$("#rsCot").text(a.cot);
-	$("#rsMon").text(a.mon);
-	$("#rsCam").text(a.cam);
+
+/** 
+ * Funcion calcular.
+ * recibe dos valores como parametros, que son utilizados para realizar los calculos
+ * */
+function calcular(x, y){
+   
+   // los valores recibidos se parsean a entero para que no hayan errores en los calculos
+   // luego se guardan en la variable
+   let cam = parseInt(x) * parseInt(y);
+
+   // Una vez realizados los calculos se muestran los resultados en la pagina
+   $("#rsCot").text(x);
+   $("#rsMon").text(y);
+   $("#rsCam").text(cam);
 
 };
